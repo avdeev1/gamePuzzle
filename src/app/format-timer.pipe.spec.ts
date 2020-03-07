3a26a034adbb24b401e7168a0f319f55ee7ad9dc
Avdey <avdeev.1van@icloud.com>
2020-03-07 13:32:38 +0300

    delete useless spec.ts files

--- src/app/format-timer.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FormatTimerPipe } from './format-timer.pipe';
-
-describe('FormatTimerPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FormatTimerPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
